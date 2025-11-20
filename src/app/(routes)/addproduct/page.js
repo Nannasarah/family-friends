3bf033e4b0b914b7f1e4bdaf2b5d214f96f7540f
Nannasarah <nannasp01@gmail.com>
2025-11-20 18:55:57 +0100

    pris input og validering

--- src/app/(routes)/addproduct/page.js
+++ src/app/(routes)/addproduct/page.js
@@ -7,14 +7,16 @@ const addProduct = () => {
   console.log(state.error?.productName);
   console.log("state", state);
   return (
-    <div className="flex h-lvh items-center justify-center">
-      <form action={postProduct}>
+    <div className="flex h-lvh flex-col items-center justify-center">
+      <form action={postProduct} className="">
         {state.error?.productName && (
           <p className="text-red-600">{state.error.productName}</p>
         )}
 
-        {state.succes && <p className="text-green-600">Din form er sendt videre!</p>}
-
+        {state.succes && (
+          <p className="text-green-600">Din form er sendt videre!</p>
+        )}
+        {/* Produktnavn */}
         <input
           type="text"
           name="productname"
@@ -22,6 +24,21 @@ const addProduct = () => {
           defaultValue={state.productName}
           className="mr-3 border-1 p-1"
         />
+
+        {/* Prisfelt */}
+        {state.error?.price && (
+          <p className="text-red-600">{state.error.price}</p>
+        )}
+        <input
+          type="number"
+          name="price"
+          inputMode="numeric" //viser kun tal på mobil
+          title="Indtast venligst kun tal (mindst ét ciffer)"
+          placeholder="Price"
+          defaultValue={state.price}
+          className="mr-3 border-1 p-1"
+        />
+
         <button
           type="submit"
           className="cursor-pointer border-1 border-blue-500 bg-blue-300 p-1 text-white"
--- src/app/action/action.js
+++ src/app/action/action.js
@@ -2,15 +2,24 @@
 export const submitProduct = async (prevState, formData) => {
   const error = {};
   const productName = formData.get("productname");
-
+  const price = formData.get("price");
+  // Validering af produktnavn
   if (!productName) {
     //hvis der ikke er et productname (!)
     error.productName = "feltet skal udfyldes";
   } else if (productName.length < 5) {
     error.productName = "Mindst 5 karakterer";
   }
+  // Validering af pris
+  if (!price) {
+    //hvis der ikke er emn price (!) ligesom med productname
+    error.price = "Pris skal udfyldes";
+    //nedenfor skal være større end 0
+  } else if (price <= 0) {
+    error.price = "Pris skal være et positivt tal";
+  }
   if (Object.keys(error).length > 0) {
-    return { error, productName };
+    return { error, productName, price };
   }
   return { succes: true }; //det her blive rkun læst hvis det ovenover ikke bliver eksikveret
 };
